c887c33f28ed874ef11b4fb6a161f8a521094397
Nic Howe <nichowe.garcia@gmail.com>
2023-09-23 18:56:57 -0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import GameLHF from '../components/games/GameLHF/Game.vue'
+</script>
+
+<template>
+  <main>
+    <canvas style="margin: auto;" id="canvas" width="640" height="480"></canvas>
+    <GameLHF/>
+  </main>
+</template>
